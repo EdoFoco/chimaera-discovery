4051c9dcb01802e546e890b7057ca39501210c61
Edoardo Foco <e.foco@hotmail.it>
2023-06-03 23:19:13 +0200

    refactor: remove uniswappish decoder

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,5 @@
-import { UniswappishDecoder } from "./UniswappishDecoder";
 import { UtilsService } from "./UtilsService";
 import { WalletService } from "./WalletService";
 import { TokenService } from "./TokenService";
 
-export { UniswappishDecoder, UtilsService,  WalletService, TokenService};
+export { UtilsService,  WalletService, TokenService};
